edf9fa0ead42c37778fb0332bfd334538976e8bb
Luke Gorrie <luke@snabb.co>
2017-03-23 10:39:01 +0000

    lj_bcread.c: Add random trailer to prototype object
    This change is intended to put some entropy into the addresses of
    bytecodes in prototypes. This will help test suites to see potential
    hash collisions and so on.
    
    The random value is read from the low bits of the CPU timestamp
    counter (TSC).

--- src/lj_bcread.c
+++ src/lj_bcread.c
@@ -336,7 +336,7 @@ GCproto *lj_bcread_proto(LexState *ls)
   ofsdbg = sizept; sizept += sizedbg;
 
   /* Allocate prototype object and initialize its fields. */
-  pt = (GCproto *)lj_mem_newgco(ls->L, (MSize)sizept);
+  pt = (GCproto *)lj_mem_newgco(ls->L, (MSize)(sizept + (__builtin_ia32_rdtsc() % 64)));
   pt->gct = ~LJ_TPROTO;
   pt->numparams = (uint8_t)numparams;
   pt->framesize = (uint8_t)framesize;
